e75753ab5a6c267650e6545eb6b0f5a7100e6e40
Rachel Jamer <rach.jamer@gmail.com>
2022-06-30 08:08:55 -0400

    Created account page
    All inputs currently display filler text and are disabled! When I get firebase working this will allow users to enter their account information

--- yarn-react-app/src/pages/Account.js
+++ yarn-react-app/src/pages/Account.js
@@ -1,8 +1,75 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {Button, Col, Container, DropdownButton, Dropdown, Form, Image, InputGroup, Row} from "react-bootstrap";
+import {MdUpload, MdSave} from "react-icons/md";
+import {auth} from "../firebase-config";
+import {useNavigate} from "react-router-dom";
+import "./Account.css";
 
-function Account() {
+function Account({isAuth}) {
+    let navigate = useNavigate();
+
+    // useEffect(() => {
+    //     if (!isAuth) {
+    //         navigate("/login"); //this is just a trick to make sure users are authenticated first
+    //     }
+    //
+    // }, [])
     return (
-        <div>Account</div>
+        <Container className="mt-5">
+            <h3>My Account</h3>
+            <Row>
+                <Col xxl={3}>
+                    <div className="d-grid mb-3">
+                        <Image thumbnail src="https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png" className="profile"/>
+                        <Button className="mt-3 profile"disabled><MdUpload/> Upload Image</Button>
+                    </div>
+
+                </Col>
+
+                <Col>
+                    <Form>
+                        <h4>Information</h4>
+                        <Row>
+                            <Form.Group as={Col}>
+                                <Form.Control type="text" placeholder="First Name" readOnly/>
+                            </Form.Group>
+                            <Form.Group as={Col}>
+                                <Form.Control type="text" placeholder="Last Name" readOnly/>
+                            </Form.Group>
+                        </Row>
+                        <Row>
+                            <Form.Group className="my-3">
+                                <Form.Control type="email" placeholder="Email" readOnly/>
+                            </Form.Group>
+                        </Row>
+                        <h4>Biography</h4>
+                        <Form.Group className="my-3">
+                            <Form.Control as="textarea" rows={3} placeholder="Bio" readOnly/>
+                        </Form.Group>
+                        <h4>Journal with Yarn</h4>
+                        <Form.Label>Enable email reminders to write stories</Form.Label>
+                        <InputGroup>
+
+                            <InputGroup.Checkbox/>
+                            <DropdownButton
+                                variant="outline-primary"
+                                title="Remind me every..."
+                                id="input-group-dropdown-4"
+                                align="end"
+                            >
+                                <Dropdown.Item>Day</Dropdown.Item>
+                                <Dropdown.Item>3 Days</Dropdown.Item>
+                                <Dropdown.Item>Week</Dropdown.Item>
+                                <Dropdown.Item>2 Weeks</Dropdown.Item>
+                                <Dropdown.Item>Month</Dropdown.Item>
+                            </DropdownButton>
+                        </InputGroup>
+
+                        <Button className="mt-3" href="/"><MdSave/> Save</Button>
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
