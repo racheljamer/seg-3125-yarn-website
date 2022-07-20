1f8efd3b7c112cf92d2b9c3b4b756c4478443c7f
Rachel Jamer <rach.jamer@gmail.com>
2022-07-19 20:37:14 -0400

    story body font size is adjustable

--- yarn-react-app/src/pages/ReadStory.js
+++ yarn-react-app/src/pages/ReadStory.js
@@ -21,16 +21,6 @@ import {GoDash} from "react-icons/go"
 import {useLocation, useParams} from "react-router-dom";
 import { doc, getDoc } from "firebase/firestore";
 
-const storyPlaceholder = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam rhoncus accumsan ante at varius. Donec odio erat, efficitur non mauris eget, egestas euismod magna. Duis ac felis ac nisl tempor venenatis vitae eget turpis. Nulla erat augue, molestie vitae neque venenatis, maximus pharetra arcu. Ut suscipit semper diam at commodo. Duis maximus ante laoreet porta maximus. Pellentesque a fermentum odio, in rutrum lectus.\n" +
-    "\n" +
-    "Nulla facilisi. Nullam metus ex, rutrum in tellus sit amet, venenatis gravida nisl. Praesent consequat, dui vitae blandit fermentum, tortor tortor mattis lectus, ac facilisis enim quam at mi. Curabitur id ullamcorper nisl. Morbi ullamcorper mauris eu turpis malesuada, vel volutpat felis venenatis. Ut eu pellentesque nisi, sit amet consectetur ipsum. Nullam ultricies, est et mattis lobortis, orci felis volutpat purus, quis sagittis nulla neque vitae dui. Aliquam a dui sed ligula suscipit venenatis. Aenean vel gravida felis. Quisque dignissim leo vitae semper sodales. Suspendisse a molestie diam. Phasellus feugiat vulputate nisi in venenatis. Maecenas scelerisque finibus sapien sit amet ultricies.\n" +
-    "\n" +
-    "Etiam nec ex arcu. Integer posuere diam eget nibh varius pellentesque eget ut nulla. Fusce sed dolor metus. Nulla nibh ligula, dictum et rhoncus at, finibus eu nibh. Donec tincidunt id nisl vel dictum. Aliquam at tellus a massa imperdiet fringilla at ut turpis. Ut odio mauris, pharetra vitae venenatis consectetur, faucibus vel ligula. Aenean ac velit quis enim bibendum viverra lacinia non magna. Mauris et nulla vestibulum, lobortis nunc vel, lacinia ante.\n" +
-    "\n" +
-    "Mauris at gravida felis, sit amet ultrices leo. Quisque vel maximus dolor. Phasellus accumsan, leo sed viverra tempus, leo augue congue ipsum, ac rhoncus leo neque id augue. Proin nec ipsum in ex commodo euismod. Nunc suscipit convallis metus, non fermentum ante tempor et. Aliquam ut dignissim eros. Sed ac tortor arcu. Etiam sit amet iaculis elit. Maecenas bibendum mi dolor, sed vulputate neque condimentum vitae. Cras placerat mauris dolor. Maecenas tempus maximus metus lacinia vestibulum. Phasellus nec mauris eget libero sodales pretium et quis felis. Duis urna elit, tempus non tellus quis, fermentum cursus ipsum. Fusce tortor mi, ultricies sit amet pretium sit amet, gravida non lorem.\n" +
-    "\n" +
-    "Donec porta eros ac mauris accumsan egestas. Aenean metus odio, convallis sed mauris convallis, efficitur volutpat orci. Donec porttitor, odio nec fringilla bibendum, risus quam iaculis sapien, iaculis feugiat magna urna eu risus. Vivamus ac fringilla lacus. In ornare interdum augue sed pulvinar. Nulla a tellus ante. Nulla molestie vehicula lobortis. Mauris sed lectus non ligula viverra finibus quis at risus. Nam molestie ipsum at lacus iaculis ornare nec ac est. Etiam at sem interdum, euismod lectus eu, tincidunt nunc. In auctor erat nec odio faucibus gravida. Suspendisse auctor enim eget massa volutpat gravida quis sit amet dui. Maecenas at tellus purus. Cras egestas dictum dolor id commodo. Nunc vel elit tellus. Duis mollis mattis massa eget cursus.";
-
 function ReadStory() {
     const {id} = useParams();
 
@@ -42,7 +32,13 @@ function ReadStory() {
 
 
     //font size change
-    const [fontSize, setFontSize] = useState(14);
+    const [fontSize, setFontSize] = useState(16);
+    const handleFontPlus = () => {
+        setFontSize(fontSize+1);
+    }
+    const handleFontMinus = () => {
+        setFontSize(fontSize-1);
+    }
 
     //Author Off-canvas
     const [showAuthor, setShowAuthor] = useState(false);
@@ -60,6 +56,7 @@ function ReadStory() {
         setShowAuthor(true);
     }
 
+    //social buttonclicks
     const handleBookmark = () => setIsBookmarked(!isBookmarked);
     const handleLike = () => setIsLiked(!isLiked);
     const handleShare = () => {
@@ -67,9 +64,8 @@ function ReadStory() {
         setIsCopied(!isCopied);
     }
 
-    //firestore get doc by id
+    //firestore get story doc by id
     const docRef = doc(db, 'teststories', id);
-
 
     useEffect(() => {
         const getStory = async() => {
@@ -86,9 +82,6 @@ function ReadStory() {
         getStory();
     }, [])
 
-
-    //const story = db.collection('teststories').doc(id).get();
-
     return (
         <Container>
             <h3 className="mt-5 text-center">{story.title}</h3>
@@ -97,13 +90,13 @@ function ReadStory() {
                 <span className="ms-auto d-inline-block align-top mb-2 ">
                     <p className="mb-0">Font Size</p>
                     <ButtonGroup>
-                        <Button variant="outline-primary" > - </Button>
-                        <Button variant="outline-primary">+</Button>
+                        <Button variant="outline-primary" onClick={handleFontMinus}> - </Button>
+                        <Button variant="outline-primary" onClick={handleFontPlus}>+</Button>
                     </ButtonGroup>
                 </span>
 
             </div>
-            <p className="content" >
+            <p className="content" style={{fontSize: fontSize}}>
                 {story.storyText}
             </p>
             <Image src={story.picture} width="50%" className="rounded my-2"/>
@@ -126,14 +119,10 @@ function ReadStory() {
                 </Button>
                 <Button className="btn" variant={isLiked ? "secondary" : "primary"} onClick={handleLike}><IoMdHeart color={isLiked ? "#e07a7f" : "white"}/> {isLiked ? "Un-l" : "L"}ike </Button>
             </div>
-
-
-
             <hr/>
             <h4>Comments</h4>
             <Comment/>
             <Comment/>
-
 
             <Offcanvas show={showAuthor} onHide={handleClose}>
                 <Offcanvas.Header closeButton>
